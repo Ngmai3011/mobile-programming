6d9a9853699687a717c564b59c8552e668839675
Mai Hoang <ngocmaiams3011@gmail.com>
2023-08-23 15:59:22 +0300

    Exercise enhanced

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import {StyleSheet, Text, View} from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello Mai</Text>
+      <Text>Hello Mai Hoang</Text>
       <StatusBar style="auto" />
     </View>
   );
